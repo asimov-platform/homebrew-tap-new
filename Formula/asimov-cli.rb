fa5510dc47f31957d638b3afe983ee8f94d99afa
imunproductive <imunproductive@gmail.com>
2025-03-21 05:34:58 +0000

    change homepage to github
    crates.io returns error 404 failing tests. look
    https://github.com/rust-lang/crates.io/issues/788

--- Formula/asimov-cli.rb
+++ Formula/asimov-cli.rb
@@ -1,6 +1,6 @@
 class AsimovCli < Formula
   desc "ASIMOV Command-Line Interface (CLI)"
-  homepage "https://crates.io/crates/asimov-cli"
+  homepage "https://github.com/asimov-platform/asimov-cli"
   url "https://github.com/imunproductive/asimov-cli/archive/refs/tags/25.0.0-dev.13.tar.gz"
   sha256 "87ad24988c4b3cb1913d9dcbb41c809975fcf15249fd1379a30b4c6fce9dfb05"
   license "Unlicense"
