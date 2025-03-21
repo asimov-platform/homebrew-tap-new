4297c02d4dd17dec785508fce6aa710b5cd16d29
imunproductive <imunproductive@gmail.com>
2025-03-21 05:11:16 +0000

    fix formula

--- Formula/asimov-cli.rb
+++ Formula/asimov-cli.rb
@@ -1,16 +1,16 @@
 class AsimovCli < Formula
-  desc "🛠️ ASIMOV Command-Line Interface (CLI)."
+  desc "ASIMOV Command-Line Interface (CLI)"
   homepage "https://crates.io/crates/asimov-cli"
   url "https://github.com/imunproductive/asimov-cli/archive/refs/tags/25.0.0-dev.13.tar.gz"
   sha256 "87ad24988c4b3cb1913d9dcbb41c809975fcf15249fd1379a30b4c6fce9dfb05"
   license "Unlicense"
 
-  depends_on "rust" => :build
-
   livecheck do
     url :stable
     strategy :github_latest
   end
+
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
